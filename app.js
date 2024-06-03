362bd7132ef77d47c0140ce4a46df8f712696fa5
Caiuuutecnologico <159969124+Caiuuutecnologico@users.noreply.github.com>
2024-06-03 19:30:32 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function exibirTexto() {
+   let textoBotao = document.querySelector('p');
+   textoBotao.textContent = 'Parabéns! Você se lascou direitinho!';
+}
+
+function reiniciar() {
+    textoBotao = document.querySelector('p');
+    textoBotao.textContent = '';
+ 
+
+}
